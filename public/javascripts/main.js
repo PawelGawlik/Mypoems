38232a890455e17253f9e996d7799edfb12ef1e5
PawelGawlik <mojaulub.6@gmail.com>
2022-11-15 21:46:28 +0100

    advertising handler

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,6 +2,16 @@ const h2 = document.querySelectorAll("h2");
 const p = document.querySelectorAll("p");
 const counter = document.querySelectorAll(".counter");
 const span = document.querySelector('span');
+const adv = document.querySelector('.advertisement');
+const a = document.querySelector('.advertisement>a');
+const off = document.querySelector('.off');
+adv.style.top = (window.innerHeight - 40) + 'px';
+const links = ["https://tmlead.pl/redirect/190176_2282", "https://tmlead.pl/redirect/190176_2289", "https://tmlead.pl/redirect/190176_2265", "https://tmlead.pl/redirect/190176_2262"];
+const desc = ["TaniaKsiążka.pl - księgarnia online", "Deichmann.com - sklep z butami online", "Lidl Polska – Bezpieczne zakupy w lidlowych cenach", "Renee.pl - Sklep z Modą"];
+const param = Math.floor(Math.random() * 4);
+const href = links[param];
+a.setAttribute('href', href);
+a.innerText = desc[param];
 fetch('/visits').then((res) => {
     return res.json();
 }).then((visitors) => {
@@ -26,10 +36,7 @@ fetch('/visits').then((res) => {
             i++;
         }
     })
+    off.onclick = () => {
+        adv.remove();
+    }
 })
-/*fetch('/visits').then((res) => {
-    res.json();
-}).then((visitors) => {
-    const numberOfVisits = visitors.visitors;
-    span.innerText = numberOfVisits;
-})*/
--- public/javascripts/poem.js
+++ public/javascripts/poem.js
@@ -6,10 +6,22 @@ const comments = document.getElementsByClassName("comments");
 const path = location.pathname;
 const param = path.split('/')[2];
 const button = document.getElementsByTagName('button');
+const adv = document.querySelector('.advertisement');
+const off = document.querySelector('.off');
+const a = document.querySelector('.advertisement>a');
 button[0].onclick = function () {
     fetch(`/likes/${param}`)
     this.remove();
 }
+off.onclick = () => {
+    adv.remove();
+}
+const links = ["https://tmlead.pl/redirect/190176_2282", "https://tmlead.pl/redirect/190176_2289", "https://tmlead.pl/redirect/190176_2265", "https://tmlead.pl/redirect/190176_2262"];
+const desc = ["TaniaKsiążka.pl - księgarnia online", "Deichmann.com - sklep z butami online", "Lidl Polska – Bezpieczne zakupy w lidlowych cenach", "Renee.pl - Sklep z Modą"];
+const param2 = Math.floor(Math.random() * 4);
+const href = links[param2];
+a.setAttribute('href', href);
+a.innerText = desc[param2];
 /*const commentDisplay = (param3) => {
     const param1 = document.createElement("p");
     const param2 = document.createTextNode(poem.comments[i][param3]);
